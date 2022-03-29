1be7f86927822264ce7dee26d125cedeccc917e5
Josh <josh@mjbrowns.com>
2022-03-29 19:21:27 -0400

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React, {useState} from 'react';
 import PageContext from "./PageContext"
+import {createTheme, ThemeProvider, styled} from "@mui/material"
 
 const App = () => {
     const [isDark, setDark] = useState(true);
     const toggleTheme = () => {
         setDark(!isDark)
     }
+    const theme = createTheme({
+        palette: {
+            mode: (isDark ? "dark" : "light")
+        }
+    })
     return (
         <>
-            <PageContext.Provider value={{
-                isDark: true,
-                toggleTheme: toggleTheme
-            }}>
-                Hello world, we are in {isDark ? "Dark" : "Light"} mode!
-            </PageContext.Provider>
+            <ThemeProvider theme={theme}>
+                <PageContext.Provider value={{
+                    isDark: true,
+                    toggleTheme: toggleTheme
+                }}>
+                    Hello world, we are in {isDark ? "Dark" : "Light"} mode!
+                </PageContext.Provider>
+            </ThemeProvider>
         </>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
