880f45d828d217fee0d8d4ca4de22d102bb2a146
= <=>
2022-04-07 19:42:01 -0500

    feat: add get user util

---
+++ src/utils/user.tsx
@@ -0,0 +1,22 @@
+export async function getUser(userid :string){
+    try{
+        const res = await fetch("http://web.localhost:8080/user/getUser", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            	userid
+            	})
+        })
+        if(res.status !== 200){
+        	const payload = await res.json()
+            throw new Error(payload.failed)
+        }
+        return (await res.json()).success
+    }
+    catch (e){
+        console.log(e)
+        return undefined
+    }
+}
