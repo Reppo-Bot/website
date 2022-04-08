17842fc48b6d401bc2c83b61ff350e971e797ff5
= <=>
2022-04-07 19:32:56 -0500

    feat: add get activity utility functions

--- src/utils/server.tsx
+++ src/utils/server.tsx
@@ -44,3 +44,73 @@ export async function getTopUsers(serverid :string, num: number){
         return undefined
     }
 }
+
+export async function getActivityForDay(serverid :string){
+    try{
+        const res = await fetch("http://web.localhost:8080/server/getActivityForDay", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            	serverid
+            	})
+        })
+        if(res.status !== 200){
+        	const payload = await res.json()
+            throw new Error(payload.failed)
+        }
+        return (await res.json()).success
+    }
+    catch (e){
+        console.log(e)
+        return undefined
+    }
+}
+
+export async function getActivityForMonth(serverid :string){
+    try{
+        const res = await fetch("http://web.localhost:8080/server/getActivityForMonth", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            	serverid
+            	})
+        })
+        if(res.status !== 200){
+        	const payload = await res.json()
+            throw new Error(payload.failed)
+        }
+        return (await res.json()).success
+    }
+    catch (e){
+        console.log(e)
+        return undefined
+    }
+}
+
+export async function getActivityForYear(serverid :string){
+    try{
+        const res = await fetch("http://web.localhost:8080/server/getActivityForYear", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            	serverid
+            	})
+        })
+        if(res.status !== 200){
+        	const payload = await res.json()
+            throw new Error(payload.failed)
+        }
+        return (await res.json()).success
+    }
+    catch (e){
+        console.log(e)
+        return undefined
+    }
+}
+
