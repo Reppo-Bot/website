7c86b9ad491e916179717704e1cf7556ae992b83
= <=>
2022-04-07 19:43:30 -0500

    feat: add get user reps util

--- src/utils/user.tsx
+++ src/utils/user.tsx
@@ -20,3 +20,26 @@ export async function getUser(userid :string){
         return undefined
     }
 }
+
+export async function getReps(userid :string){
+    try{
+        const res = await fetch("http://web.localhost:8080/user/getReps", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userid
+                })
+        })
+        if(res.status !== 200){
+            const payload = await res.json()
+            throw new Error(payload.failed)
+        }
+        return (await res.json()).success
+    }
+    catch (e){
+        console.log(e)
+        return undefined
+    }
+}
