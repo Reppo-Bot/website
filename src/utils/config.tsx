fed5f40b2f0dc5326b593e2df322f2871d7b365b
= <=>
2022-04-07 20:33:31 -0500

    feat: add update and remove for config

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -42,3 +42,51 @@ export async function getBots(token: string){
         return undefined
     }
 }
+
+export async function addServer(token: string, serverid: string){
+    try{
+        const res = await fetch("http://web.localhost:8080/private/addServer", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                serverid
+            })
+        })
+        if(res.status !== 200){
+            const payload = await res.json()
+            throw new Error(payload.failed)
+        }
+        return (await res.json()).success
+    }
+    catch (e){
+        console.log(e)
+        return undefined
+    }
+}
+
+export async function removeServer(token: string, serverid: string){
+    try{
+        const res = await fetch("http://web.localhost:8080/private/removeServer", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                serverid
+            })
+        })
+        if(res.status !== 200){
+            const payload = await res.json()
+            throw new Error(payload.failed)
+        }
+        return (await res.json()).success
+    }
+    catch (e){
+        console.log(e)
+        return undefined
+    }
+}
